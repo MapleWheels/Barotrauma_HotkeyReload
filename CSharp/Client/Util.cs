316a83cce5002d3c34b91cd20124735d8f83b5b7
TBN_MapleWheels <njainanan@gmail.com>
2022-11-23 12:03:35 -0500

    Added most of the implementation of the Refill to player inventory.

--- CSharp/Client/Util.cs
+++ CSharp/Client/Util.cs
@@ -130,6 +130,27 @@ internal static class Util
         }
         return pref.Any() ? pref : compat;
     }
+    
+    internal static List<Item> FindAllCompatWithPreference(this Inventory container, Character targetCharacter,
+        ItemPrefab? prefab = null, int slotIndex = -1, Func<Item, bool>? predicate = null)
+    {
+        List<Item> compat = new();
+        List<Item> pref = new();
+        List<Item> iterList = new();
+
+        iterList.BuildIterList(container.AllItemsMod, true, predicate); //No preference, just make the list
+
+        foreach (Item item in iterList)
+        {
+            if (!item.CompatibleWithInv(targetCharacter, slotIndex))
+                continue;
+            if (prefab is null || item.Prefab.Identifier.Equals(prefab.Identifier))
+                pref.Add(item);
+            else
+                compat.Add(item);
+        }
+        return pref.Any() ? pref : compat;
+    }
 
     internal static Item? FindCompatWithPreference(this CharacterInventory container, Item targetContainer, 
         ItemPrefab? prefab = null, int slotIndex = -1, Func<Item, bool>? predicate = null)
@@ -146,7 +167,6 @@ internal static class Util
         
         foreach (Item item in iterList)
         {
-            DebugConsole.LogError($"FindCompat: {item.Name}");
             if (!item.CompatibleWithInv(targetContainer, slotIndex))
                 continue;
             if (prefab is null || item.Prefab.Identifier.Equals(prefab.Identifier)) 
@@ -166,8 +186,26 @@ internal static class Util
         
         foreach (Item item in iterList)
         {
-            DebugConsole.LogError($"FindCompat: {item.Name}");
             if (!item.CompatibleWithInv(targetContainer, slotIndex))
+                continue;
+            if (prefab is null || item.Prefab.Identifier.Equals(prefab.Identifier)) 
+                return item;
+            foundItem = item;
+        }
+        return foundItem;
+    }
+    
+    internal static Item? FindCompatWithPreference(this Inventory container, Character targetCharacter, 
+        ItemPrefab? prefab = null, int slotIndex = -1, Func<Item, bool>? predicate = null)
+    {
+        Item? foundItem = null;
+        List<Item> iterList = new();
+
+        iterList.BuildIterList(container.AllItemsMod, true, predicate);
+        
+        foreach (Item item in iterList)
+        {
+            if (!item.CompatibleWithInv(targetCharacter, slotIndex))
                 continue;
             if (prefab is null || item.Prefab.Identifier.Equals(prefab.Identifier)) 
                 return item;
@@ -210,21 +248,19 @@ internal static class Util
         return items;
     }
 
-
-    #warning TODO: Implement Refilling Character Inventory slots.
+    
     internal static void RefillItemStacksUsingContainer(this Character target, Inventory sourceContainer,
         bool recursive = true)
     {
-        throw new NotImplementedException();
-    }
-
-    internal static void RefillItemStacksUsingContainer(this Item target, Inventory sourceContainer, bool recursive = true)
-    {
-        if (target.OwnInventory is null)
+        if (target.Inventory is null
+            || target.Inventory.Locked
+            || sourceContainer.Locked
+            || sourceContainer.Capacity < 1
+            || !sourceContainer.AllItemsMod.Any())
             return;
-        
-        Inventory targetContainer = target.OwnInventory;
-        
+
+        CharacterInventory targetContainer = target.Inventory;
+
         for (int index = 0; index < targetContainer.Capacity; index++)
         {
             //try to fill nested inventory items first
@@ -233,9 +269,7 @@ internal static class Util
                 foreach (Item item in targetContainer.GetItemsAt(index).ToImmutableList())
                 {
                     if (item is { OwnInventory.Capacity: > 0})
-                    {
                         item.RefillItemStacksUsingContainer(sourceContainer, recursive);
-                    }
                 }
             }
             //fill self
@@ -259,6 +293,52 @@ internal static class Util
             }
         }
     }
+
+    internal static void RefillItemStacksUsingContainer(this Item target, Inventory sourceContainer, bool recursive = true)
+    {
+        if (target.OwnInventory is null
+            || target.OwnInventory.Locked
+            || sourceContainer.Locked
+            || sourceContainer.Capacity < 1
+            || !sourceContainer.AllItemsMod.Any())
+            return;
+        
+        Inventory targetContainer = target.OwnInventory;
+        
+        for (int index = 0; index < targetContainer.Capacity; index++)
+        {
+            //try to fill nested inventory items first
+            if (recursive)
+            {
+                foreach (Item item in targetContainer.GetItemsAt(index).ToImmutableList())
+                {
+                    if (item is { OwnInventory.Capacity: > 0})
+                        item.RefillItemStacksUsingContainer(sourceContainer, recursive);
+                }
+            }
+            //fill self
+            if (targetContainer.GetItemAt(index) is { } item1)
+            {
+                int diff = target.GetSlotMaxStackSize(item1, index) - targetContainer.GetItemsAt(index).Count();
+                if (diff > 0)
+                {
+                    List<Item> refillItems = sourceContainer.FindAllCompatWithPreference(target, item1.Prefab, index,
+                        item2 => item2.Prefab.Identifier.Equals(item1.Prefab.Identifier));
+                    foreach (Item refillItem in refillItems)
+                    {
+                        if (targetContainer.TryPutItem(refillItem, index, false, true, Character.Controlled))
+                            diff--;
+                        else
+                            break;  //ain't working out or max stack(?)
+                        if (diff < 1)
+                            break;
+                    }
+                }
+            }
+        }
+    }
+    
+    
 
     internal static readonly Barotrauma.InvSlotType[] LimbsSearchOrder = 
     {
