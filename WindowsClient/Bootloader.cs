5972a838c235e668587db06b6049b6ab1830d0a5
MapleWheels <njainanan@gmail.com>
2023-02-15 18:17:57 -0500

    Refactor of mod structure. - Untested new WindowsClient csproj. Untested behaviour change for Quick Loot All to not try to put items into the player's hands if they are not holding a container.

---
+++ WindowsClient/Bootloader.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Immutable;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using Barotrauma;
+using HarmonyLib;
+using Microsoft.Xna.Framework.Input;
+using ModdingToolkit;
+using ModdingToolkit.Patches;
+using ModdingToolkit.Config;
+
+[assembly: IgnoresAccessChecksTo("Barotrauma")]
+
+namespace HotkeyReload;
+
+sealed class Bootloader : IAssemblyPlugin
+{
+    internal static IConfigControl?
+        KeybindReload,
+        KeybindQuickLootAll,
+        KeybindQuickStackToPlayer,
+        KeybindQuickStackToStorage;
+
+    private void RegisterConfig()
+    {
+        KeybindReload = ConfigManager.AddConfigKeyOrMouseBind(
+            "Reload",
+            "HotkeyReload",
+            new KeyOrMouse(Keys.R)
+        );
+        
+        KeybindQuickLootAll = ConfigManager.AddConfigKeyOrMouseBind(
+            "QuickLootAll",
+            "HotkeyReload",
+            new KeyOrMouse(Keys.H)
+        );
+        
+        KeybindQuickStackToPlayer = ConfigManager.AddConfigKeyOrMouseBind(
+            "QuickStackPlayer",
+            "HotkeyReload",
+            new KeyOrMouse(Keys.K)
+        );
+        
+        KeybindQuickStackToStorage = ConfigManager.AddConfigKeyOrMouseBind(
+            "QuickStackStorage",
+            "HotkeyReload",
+            new KeyOrMouse(Keys.L)
+        );
+    }
+
+    private void RegisterPatches()
+    {
+        PatchManager.RegisterPatches(new List<PatchManager.PatchData>()
+        {
+            new PatchManager.PatchData(
+                AccessTools.Method(typeof(Barotrauma.LuaCsSetup), "Update"),
+                null,
+                new HarmonyMethod(AccessTools.Method(typeof(P_LuaCsSetup_Update), "Postfix"))
+                )
+        });
+    }
+
+    public void Initialize()
+    {
+        RegisterConfig();
+        RegisterPatches();
+    }
+
+    public void OnLoadCompleted()
+    {
+    }
+
+    public PluginInfo GetPluginInfo() => new PluginInfo("Hotkey Reload", "2.0", ImmutableArray<string>.Empty);
+
+    public void Dispose()
+    {
+    }
+}
---
+++ WindowsClient/QuickActions.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using Barotrauma;
+using Barotrauma.Extensions;
+using Barotrauma.Items.Components;
+using HarmonyLib;
+
+namespace HotkeyReload;
+
+public static class QuickActions
+{
+    private static readonly InvSlotType[] InvPriorityOrder =
+    {
+        InvSlotType.LeftHand, InvSlotType.RightHand,
+        InvSlotType.Bag, InvSlotType.InnerClothes,
+        InvSlotType.OuterClothes, InvSlotType.Head,
+        InvSlotType.None
+    };
+    
+    /// <summary>
+    /// Tries to put all items of an inventory a player is interacting with into the player's inventory.
+    /// Prioritizes any storages the player is holding in their hands, then most of their limb slots, then their hot bar.
+    /// </summary>
+    public static void QuickLootAllToPlayerInventory()
+    {
+        if (!IsReadyToExchangeItems(Character.Controlled))
+            return;
+
+        CharacterInventory charInv = Character.Controlled.Inventory;
+        
+        //SelectedItem Inv
+        Item selectedItem = Character.Controlled.SelectedItem;
+        
+        //Is the selected item in their hands?
+        if (selectedItem == charInv.GetItemInLimbSlot(InvSlotType.LeftHand)
+            || selectedItem == charInv.GetItemInLimbSlot(InvSlotType.RightHand))
+            return;
+
+        foreach (InvSlotType slotType in InvPriorityOrder)
+        {
+            if (slotType is InvSlotType.LeftHand or InvSlotType.RightHand or InvSlotType.Bag)
+            {
+                if (charInv.GetItemInLimbSlot(slotType) is { OwnInventory: { Capacity: > 0, Locked: false } } item)
+                {
+                    foreach (Item item1 in selectedItem.OwnInventory.AllItemsMod)
+                    {
+                        item.OwnInventory.TryPutItem(item1, Character.Controlled);
+                    }
+                    continue;
+                }
+            }
+
+            if (slotType is not (InvSlotType.LeftHand or InvSlotType.RightHand))
+            {
+                foreach (Item item in selectedItem.OwnInventory.AllItemsMod)
+                {
+                    charInv.TryPutItem(item, Character.Controlled, new List<InvSlotType> { slotType });
+                }
+            }
+        }
+        
+        
+    }
+    
+    /// <summary>
+    /// Tries to refill the stacks of all items of the storage inventory a player is interacting with using
+    /// items in the player's inventory.
+    /// </summary>
+    public static void QuickStackToStorageInventory()
+    {
+        if (!IsReadyToExchangeItems(Character.Controlled))
+            return;
+        Character.Controlled.SelectedItem.RefillItemStacksUsingContainer(Character.Controlled.Inventory);
+    }
+
+    /// <summary>
+    /// Tries to refill the stacks of all items in the player's inventory using the items in the storage
+    /// inventory the player is interacting with.
+    /// </summary>
+    public static void QuickStackToPlayerInventory()
+    {
+        if (!IsReadyToExchangeItems(Character.Controlled))
+            return;
+        Character.Controlled.RefillItemStacksUsingContainer(Character.Controlled.SelectedItem.OwnInventory);
+    }
+
+    private static bool IsReadyToExchangeItems(Character character)
+    {
+        return Util.CheckIfValidToInteract()
+               && Util.CheckIfCharacterReady(character)
+               && character.SelectedItem is { OwnInventory.Capacity: > 0 };
+    }
+    
+}
